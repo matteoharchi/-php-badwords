089380a29e62ee6751a885a752a5d6aa98443d21
Matteo Harchi <matteo.harchi@gmail.com>
2020-09-15 19:50:25 +0200

    fatto con get e newline

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <!-- Creare una variabile con un paragrafo di testo.
 Visualizzare a schermo il paragrafo con la relativa lunghezza e sostituire la badword passata in GET con tre *. -->
-<?php
+
+<!-- SENZA GET -->
+ <!-- <?php
     $paragrafo = "Nel mezzo del cammin di nostra vita, mi ritrovai in una Selva Oscura, che la diritta via era smarrita.";
     $badWord = str_replace("vita", "***", $paragrafo);
     $length= "Lunghezza:" . " " . strlen($paragrafo) . "caratteri";
     echo $badWord;
+    echo $length;
 
-?>
+?>; -->
 
+<!-- CON GET -->
 <?php
-echo $length;
-?>
+    $paragrafo2 = "Nel mezzo del cammin di nostra vita, mi ritrovai in una Selva Oscura, che la diritta via era smarrita.";
+    $badWord2 = $_GET['word'];
+    $nuovoParagrafo = str_replace($badWord2, "***", $paragrafo2);
+    $length= "Lunghezza:" . " " . strlen($paragrafo) . "caratteri";
+    echo nl2br($nuovoParagrafo . "\n" . $length);
+?>;
